8255bace0c7c8cd0fdcfcabc3157a6906c1b17d1
Flavie Bourcet <flavie.bourcet@gmail.com>
2024-05-22 15:40:30 +0200

    Fix : bills proof displayed

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,33 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
+import { screen } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
+import NewBillUI from "../views/NewBillUI.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    test("Then I should be able to pass 'png', 'jpg' or 'jpeg' as a Proof", () => {
+      const user = userEvent;
+
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      const fileInput = screen.getByTestId("file");
+      const files = [
+        new File(["test1"], "test1.png", { type: "image/png" }),
+        new File(["test2"], "test2.jpg", { type: "image/jpg" }),
+        new File(["test3"], "test3.jpeg", { type: "image/jpeg" }),
+      ];
+
+      files.forEach((file) => {
+        user.upload(fileInput, file);
+        console.log(fileInput.files[0].type);
+
+        expect(fileInput.files[0]).toBe(file);
+        expect(fileInput.files.item(0)).toBe(file);
+        expect(fileInput.files[0].type).toBe(file.type);
+        expect(fileInput.files).toHaveLength(1);
+      });
+    });
+  });
+});
