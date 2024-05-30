c1675371301f4fe87ab9636c43f87eb54e37c025
Flavie Bourcet <flavie.bourcet@gmail.com>
2024-05-30 16:59:50 +0200

    NewBills adding test for checking the form is displayed and the vertical icon highlighted

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,9 +2,44 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom";
 import userEvent from "@testing-library/user-event";
 import NewBillUI from "../views/NewBillUI.js";
+import { screen, waitFor } from "@testing-library/dom";
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+
+import router from "../app/Router.js";
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then the newBill form should be displayed", async () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+    });
+
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await waitFor(() => screen.getByTestId("icon-mail"));
+      const mailIcon = screen.getByTestId("icon-mail");
+      //to-do write expect expression
+      expect(mailIcon.classList.contains("active-icon")).toBe(true);
+    });
+  });
+});
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
